be2d79c9076bef0cc9bfa2b7de5516141f8b1677
sun <thebthebtheb1234@gmail.com>
2023-11-12 13:20:57 +0900

    jwt 관련 config 내용 appmodule로 분리

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,20 +7,11 @@ import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import * as Joi from '@hapi/joi';
+import { AppConfigModule } from './config/config.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      validationSchema: Joi.object({
-        POSTGRES_HOST: Joi.string().required(),
-        POSTGRES_PORT: Joi.number().required(),
-        POSTGRES_USER: Joi.string().required(),
-        POSTGRES_PASSWORD: Joi.string().required(),
-        POSTGRES_DB: Joi.string().required(),
-        JWT_ACCESS_TOKEN_SECRET: Joi.string().required(),
-        JWT_ACCESS_TOKEN_EXPIRATION_TIME: Joi.string().required(),
-      }),
-    }),
+    AppConfigModule,
     ProductModule,
     DatabaseModule,
     UserModule,
